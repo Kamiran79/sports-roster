8b66c8da04dafd6e026febbb4a552982abfbb253
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-08-20 19:36:00 -0500

    Auth and DB Finished

---
+++ src/components/Team/Team.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './Team.scss';
+
+class Team extends React.Component {
+  render() {
+    return (
+      <div className="Team">
+        <h2>INSIDE Team COMPONENT</h2>
+        <button className="btn btn-info">I am a button</button>
+      </div>
+    );
+  }
+}
+
+export default Team;
